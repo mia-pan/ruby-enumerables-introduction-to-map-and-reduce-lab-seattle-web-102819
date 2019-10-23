b62a442e14875eaf0515c5c547b881e119286379
Mia Pan <miapanyuewei@gmail.com>
2019-10-23 03:48:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,61 @@
 # My Code here....
+def map_to_negativize(array)
+  new=[]
+  i=0 
+  while i<array.length do 
+    new.push(array[i]*-1)
+    i+=1
+  end
+  return new 
+end
+def map_to_no_change(array)
+  new=[]
+  i=0 
+  while i<array.length do 
+    new.push(array[i])
+    i+=1
+  end
+  return new 
+end
+def map_to_double(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push( array[i] * 2 )
+    i += 1
+  end
+  return new
+end
+def map_to_square(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new.push( array[i] **2 )
+    i += 1
+  end
+  return new
+end
+def reduce_to_total(array, starting_point=0)
+  new = starting_point
+  i = 0
+  while i < array.length do
+    new += array[i]
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_all_true(array)
+  i = 0
+  while i < array.length do
+    reduce_to_all_true(array)[i]
+    i+=1
+ 
+return true
+end
+  
+end
+
+
+
+
