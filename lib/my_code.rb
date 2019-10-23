91cb5be4b27db443441b10590f2e80050dcc2704
Mia Pan <miapanyuewei@gmail.com>
2019-10-23 04:12:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,15 +46,13 @@ def reduce_to_total(array, starting_point=0)
 end
 
 def reduce_to_all_true(array)
-  i=0
-  while i < array.length do 
-    return false
-    if (array[i])
-      i +=1
-    end
-  return TRUE
+  i = 0
+  while i < array.length do
+    return false 
+    if array[i]
+    i += 1
   end
-
+  return true
 end
 
 
